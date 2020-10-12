2f83047eab2609e222c72bec73397ed1cbc28b8b
Stanislao <stanislaoweb@libero.it>
2020-10-12 23:16:45 +0200

    finito

--- main.js
+++ main.js
@@ -6,4 +6,4 @@ var cognomeUtente = prompt('Qual\' è il tuo cognome?');
 
 var colorePreferito = prompt('Qual\'è il tuo colore preferito?');
 
-document.getElementById('password').innerHTML = 'Ciao! Ecco la tua password = ' + nomeUtente + cognomeUtente + colorePreferito + '!';
+document.getElementById('password').innerHTML = nomeUtente + cognomeUtente + colorePreferito + '!';
